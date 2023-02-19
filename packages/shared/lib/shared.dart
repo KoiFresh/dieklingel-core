7cb8db4212be3f40a389847670111315efffef94
Kai Mayer <kai.mayer@t-online.de>
2023-02-19 22:46:24 +0000

    👌 feat: export shared library classes

--- packages/shared/lib/shared.dart
+++ packages/shared/lib/shared.dart
@@ -1,7 +1,13 @@
 library shared;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'bloc/bloc_provider.dart';
+export 'bloc/bloc.dart';
+export 'bloc/multi_bloc_provider.dart';
+
+export 'blocs/mqtt_client_bloc.dart';
+
+export 'models/ice_server.dart';
+export 'models/mqtt_uri.dart';
+
+export 'mqtt/mqtt_client_factory.dart';
+export 'mqtt/mqtt_client_state.dart';
