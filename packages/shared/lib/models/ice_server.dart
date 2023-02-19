08e4b0fc96af8ba6471ff019ea6c0a195d1f3f48
Kai Mayer <kai.mayer@t-online.de>
2023-02-19 22:41:27 +0000

    📦 new: add shared classes

---
+++ packages/shared/lib/models/ice_server.dart
@@ -0,0 +1,47 @@
+import 'package:yaml/yaml.dart';
+
+class IceServer {
+  String urls;
+  String username;
+  String credential;
+
+  IceServer({
+    required this.urls,
+    this.username = "",
+    this.credential = "",
+  });
+
+  factory IceServer.fromYaml(YamlMap yaml) {
+    return IceServer(
+      urls: yaml["urls"] ?? "",
+      username: yaml["username"] ?? "",
+      credential: yaml["credential"] ?? "",
+    );
+  }
+
+  @override
+  String toString() {
+    return "urls: $urls; username: $username; credential: $credential";
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      urls: urls,
+      username: username,
+      credential: credential,
+    };
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (other is! IceServer) {
+      return false;
+    }
+    return urls == other.urls &&
+        username == other.username &&
+        credential == other.credential;
+  }
+
+  @override
+  int get hashCode => Object.hash(urls, username, credential);
+}
