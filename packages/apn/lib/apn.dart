a57760db171fe24327c6a50446bbf5965ac57a74
Kai Mayer <kai.mayer@t-online.de>
2023-02-21 10:14:38 +0000

    👌 feat: read settings from config

--- packages/apn/lib/apn.dart
+++ packages/apn/lib/apn.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'package:apn/config.dart';
 import 'package:http/http.dart' as http;
+import 'package:yaml/yaml.dart';
+import 'package:interpolation/interpolation.dart';
 
 // TODO(KoiFresh): import dart_shared
 import 'hive/registry_entry_adapter.dart';
@@ -11,17 +15,15 @@ import 'package:hive/hive.dart';
 import 'package:path/path.dart' as p;
 
 class Apn {
-  // TODO(KoiFresh): read from config file
-  MqttUri uri = MqttUri(
-    host: "server.dieklingel.com",
-    port: 1883,
-    channel: "com.dieklingel/mayer/kai/",
-  );
+  late MqttUri uri;
+  late Uri worker;
+  late String title;
+  late String body;
+
   MqttClientBloc mqtt = MqttClientBloc();
 
   void main() async {
     stdout.writeln("APN: App Push Notification");
-    mqtt.uri.add(uri);
 
     //Directory directory = await getApplicationDocumentsDirectory();
     String path = p.join(
@@ -32,8 +34,24 @@ class Apn {
     Hive.registerAdapter(RegistryEntryAdapter());
     await Hive.openBox<RegistryEntry>("apn");
 
+    await setupMqttChannels();
+
+    YamlMap config = await getConfig();
+    uri = MqttUri.fromUri(
+      Uri.parse(
+        config["mqtt"]?["uri"] ?? "mqtt://127.0.0.1:1883/com.dieklingel/",
+      ),
+    );
+    // TODO: create error message, if key does not exists
+    worker = Uri.parse(config["apn"]?["url"]);
+    title = config["apn"]?["title"] ?? "no title :(";
+    body = config["apn"]?["body"] ?? "no body :(";
+
+    mqtt.uri.add(uri);
+  }
+
+  Future<void> setupMqttChannels() async {
     mqtt.watch("/io/action/sign/clicked").listen((event) async {
-      String topic = event.key;
       String message = event.value;
 
       Box<RegistryEntry> box = Hive.box<RegistryEntry>("apn");
@@ -78,17 +96,39 @@ class Apn {
   }
 
   Future<void> sendPushNotification(List<RegistryEntry> entries) async {
-    Uri url = Uri.https("fcm-worker.dieklingel.workers.dev", "/fcm/send");
+    if (entries.isEmpty) {
+      return;
+    }
 
-    // TODO(KoiFresh): allow interpolation with title and body
+    DateTime time = DateTime.now();
+    Interpolation interpolation = Interpolation();
+
+    String title = interpolation.eval(this.title, {
+      "YEAR": time.year.toString(),
+      "MONTH": time.month.toString().padLeft(2, "0"),
+      "DAY": time.day.toString().padLeft(2, "0"),
+      "HOUR": time.hour.toString().padLeft(2, "0"),
+      "MINUTE": time.minute.toString().padLeft(2, "0"),
+      "SECOND": time.second.toString().padLeft(2, "0"),
+    });
+
+    String body = interpolation.eval(this.body, {
+      "YEAR": time.year.toString(),
+      "MONTH": time.month.toString().padLeft(2, "0"),
+      "DAY": time.day.toString().padLeft(2, "0"),
+      "HOUR": time.hour.toString().padLeft(2, "0"),
+      "SECOND": time.second.toString().padLeft(2, "0"),
+    });
+
     Map<String, dynamic> payload = {
       "tokens": entries.map((e) => e.token).toList(),
-      "title": "New Notification!",
-      "body": "Someone has rang",
+      "title": title,
+      "body": body,
       "image": "",
     };
 
     // TODO(KoiFresh): delete entries, if response is not ok
-    await http.post(url, body: jsonEncode(payload));
+    await http.post(worker, body: jsonEncode(payload));
+    // TODO: log response
   }
 }
