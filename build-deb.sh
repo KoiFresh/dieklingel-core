e6bc1774920ba6201872b8d211996398a4bc677d
Kai Mayer <kai.mayer@t-online.de>
2023-05-24 21:44:47 +0000

    feat: add comments and root dir var

--- build-deb.sh
+++ build-deb.sh
@@ -2,9 +2,15 @@
 
 ROOT_DIRECTORY=$(pwd)
 
+#################################################################
+# Build the core package and copy into deb package build folder #
+#################################################################
+
+cd $ROOT_DIRECTORY
+
 flutter build linux
 
-BUILD_DIRECTORY=.deb-build
+BUILD_DIRECTORY=$ROOT_DIRECTORY/.deb-build
 BUILD_ARCHITECTURE=arm64
 
 rm -rf $BUILD_DIRECTORY
@@ -14,6 +20,10 @@ mkdir -p $BUILD_DIRECTORY/$BUILD_ARCHITECTURE/usr/share/dieklingel/exec
 
 cp -r deb/$BUILD_ARCHITECTURE/* $BUILD_DIRECTORY/$BUILD_ARCHITECTURE
 cp -r build/linux/$BUILD_ARCHITECTURE/release/bundle/* $BUILD_DIRECTORY/$BUILD_ARCHITECTURE/usr/share/dieklingel/core
+
+####################################################################################################
+# Build the .deb package from $BUILD_DIRECTORY/$BUILD_ARCHITECTURE (e.g .deb-build/arm64) folder.  #
+####################################################################################################
 
 DEB_PACKAGE_VERSION=$(cat $BUILD_DIRECTORY/$BUILD_ARCHITECTURE/DEBIAN/control | grep -Po '(?<=Version:\s)(.*)') 
 
