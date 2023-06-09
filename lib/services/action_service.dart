0ddb8a0ea667fc2f6778cfb265a035c57b178d31
Kai Mayer <kai.mayer@t-online.de>
2023-06-09 08:45:27 +0000

    feat: add blueprint

--- lib/services/action_service.dart
+++ lib/services/action_service.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
+import 'package:blueprint/blueprint.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
+import 'package:shell/shell.dart';
 
 import '../models/action.dart';
 import '../repositories/action_repository.dart';
@@ -27,6 +29,46 @@ class ActionService {
 
       return Response.ok(
         jsonEncode(body),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      );
+    });
+
+    router.post("/", (Request request) async {
+      Map<String, dynamic> body;
+      String trigger;
+      Map<String, dynamic> environment;
+      try {
+        body = jsonDecode(await request.readAsString());
+
+        matchMap(
+          body,
+          {
+            "trigger": StringF,
+            "environment": MapOrNull,
+          },
+          throwable: true,
+        );
+
+        trigger = body["trigger"];
+        environment = body["environment"];
+      } on FormatException catch (error) {
+        return Response.badRequest(body: error.toString());
+      } on TypeDoesNotMatch catch (error) {
+        return Response.badRequest(body: error.toString());
+      }
+
+      List<Action> actions = await actionRepository.fetchActionsFor(trigger);
+
+      for (Action action in actions) {
+        Shell shell = Shell(environment: environment.cast<String, String>());
+
+        await shell.run("eval", arguments: [action.lane]);
+      }
+
+      return Response.ok(
+        jsonEncode(actions.map((e) => e.toMap()).toList()),
         headers: {
           "Content-Type": "application/json",
         },
